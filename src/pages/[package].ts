4c667db88a4323eafd00d1e5ab95cb5f029d8c54
Anirudh Oppiliappan <x@icyphox.sh>
2024-11-25 14:18:08 +0200

    misc

--- src/pages/[package].ts
+++ src/pages/[package].ts
@@ -3,15 +3,23 @@ import type { APIRoute } from 'astro'
 export const GET: APIRoute = ctx => {
   const isGoGet = ctx.url.searchParams.get('go-get') == '1'
   if (!isGoGet) {
-    return new Response(`Page Not found`, {
+    return new Response(`not found`, {
       status: 404,
     })
   }
   return new Response(
-    `<meta
+    `
+    <html>
+    <head>
+    <meta
       name="go-import"
       content="kapycluster.com git https://github.com/kapycluster/corp"
-    />`,
+    />
+    </head>
+    <body>
+    <h1>Hello, Go!</h1>
+    </body>
+    </html>`,
     {
       status: 200,
       headers: {
