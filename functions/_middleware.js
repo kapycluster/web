a5746f0a71229ef209072dfe9d5caf7cf6caba65
Anirudh Oppiliappan <x@icyphox.sh>
2024-11-25 06:09:37 +0200

    don't 404 for now

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,29 +1,23 @@
 export function onRequest({ request }) {
-  const url = new URL(request.url);
-  
+  const url = new URL(request.url)
+
   if (url.searchParams.get('go-get') === '1') {
-    return new Response(`<!DOCTYPE html>
-<html>
+    return new Response(
+      `<html>
 <head>
     <meta name="go-import" content="kapycluster.com/corp git https://github.com/kapycluster/corp">
 </head>
 <body>
     <p>Are you Go?</p>
 </body>
-</html>`, {
-      headers: {
-        'content-type': 'text/html;charset=UTF-8',
-      },
-    });
+</html>`,
+      {
+        headers: {
+          'content-type': 'text/html;charset=UTF-8',
+        },
+      }
+    )
   }
 
-  return new Response(`<!DOCTYPE html>
-  <html>
-  <body>
-    <p>Four oh four!</p>
-  </body>
-  </html>`, {
-    status: 404,
-  });
+  return Response.redirect('/', 302)
 }
-
